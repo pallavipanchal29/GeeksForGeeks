a08c93a14c8ad3aa9f51ddaacd448108e03b0c7f
pallavi panchal <panchal.pallavi29@gmail.com>
2021-05-01 16:13:58 +0530

    count Zeroes

---
+++ src/easy/CountZeroes.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountZeroes {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] input = br.readLine().split(" ");
+        int[] a = new int[n];
+        for (int j = 0; j < n; j++) {
+            a[j] = Integer.parseInt(input[j]);
+        }
+
+        System.out.println(countZeroes(a, n));
+    }
+
+    private static int countZeroes(int[] arr, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0)
+                count++;
+        }
+        return count;
+    }
+}
