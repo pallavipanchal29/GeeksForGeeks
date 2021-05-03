074c5d25d61704564ba8dbec9661b697e8493fc5
pallavi panchal <panchal.pallavi29@gmail.com>
2021-05-03 11:42:45 +0530

    Convert array to zigzag array

---
+++ src/basic/ArrayToZigzagArray.java
@@ -0,0 +1,45 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArrayToZigzagArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] input = br.readLine().split(" ");
+        int[] a = new int[n];
+        for (int j = 0; j < n; j++) {
+            a[j] = Integer.parseInt(input[j]);
+        }
+
+        zigZag(a, n);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print((a[i] + " "));
+        }
+    }
+
+    static void zigZag(int[] arr, int n) {
+        boolean flag = true;
+        int temp = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (flag) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i] < arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+            flag = !flag;
+        }
+    }
+}
