9365c4df6b403a22f154361a6e74d26430022e5e
pallavi panchal <panchal.pallavi29@gmail.com>
2021-05-03 23:23:53 +0530

    K th smallest element

---
+++ src/medium/KthSmallestElement.java
@@ -0,0 +1,28 @@
+package medium;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class KthSmallestElement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] temp = br.readLine().split(" ");
+        int n = Integer.parseInt(temp[0]);
+        int k = Integer.parseInt(temp[1]);
+
+        String[] input = br.readLine().split(" ");
+        int[] a = new int[n];
+        for (int j = 0; j < n; j++) {
+            a[j] = Integer.parseInt(input[j]);
+        }
+
+        System.out.println(kthSmallest(a, 0, n - 1, k));
+    }
+
+    public static int kthSmallest(int[] arr, int l, int r, int k) {
+        Arrays.sort(arr);
+        return arr[k - 1];
+    }
+}
