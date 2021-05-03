df41c206eedb9a2069be5e4671f6f2736920b4c6
pallavi panchal <panchal.pallavi29@gmail.com>
2021-05-03 10:54:57 +0530

    Equilibrium point Index in array

--- src/easy/EquilibriumPointIndex.java
+++ src/easy/EquilibriumPointIndex.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class EquilibriumPoint {
+public class EquilibriumPointIndex {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tests = Integer.parseInt(br.readLine());
@@ -23,6 +23,7 @@ public class EquilibriumPoint {
     }
 
     static int findEquilibrium(int[] A, int n) {
+
         int sum = 0;
         int leftSum = 0;
         for (int i = 0; i < n; i++) {
