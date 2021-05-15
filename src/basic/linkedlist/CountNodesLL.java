d2e8a37401a9f0411d8fbf9dfa05f35f7427c3c0
pallavi panchal <panchal.pallavi29@gmail.com>
2021-05-15 10:40:44 +0530

    Count nodes of linked list

---
+++ src/basic/linkedlist/CountNodesLL.java
@@ -0,0 +1,32 @@
+package basic.linkedlist;
+
+import java.util.Scanner;
+
+public class CountNodesLL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+
+            System.out.println(getCount(head));
+        }
+    }
+
+    public static int getCount(Node head) {
+        int count = 0;
+
+        if (head == null) return 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+}
